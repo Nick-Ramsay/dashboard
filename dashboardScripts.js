79ddc8eeff0ee960d31853a0506966d4f904b1f5
Nick Ramsay <NickRamsay1989@gmail.com>
2019-05-26 12:17:39 +1000

    Added code to HTML, CSS, and JS docs to enable modal... finally!

--- dashboardScripts.js
+++ dashboardScripts.js
@@ -72,9 +72,33 @@ function startSydneyTime() {
     h + ":" + m+" "+ampm/* + ":" + s*/;
     var t = setTimeout(startSydneyTime, 1830);
 }
+
 function checkSydneyTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
 
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
+function aboutModal() {
+    //Gets the modal
+    var modal = document.getElementById("aboutModal");
+    //Gets the button that opens the modal
+    var btn = document.getElementById("aboutBtn");
+    //Get the <span> element that closes modal
+    var span = document.getElementsByClassName("modalClose")[0];
+    //When the user clicks the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+}
