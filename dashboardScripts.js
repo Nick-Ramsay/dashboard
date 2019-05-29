4cb3cd3dd2a0023cc88115583452e3cf12f2d6cd
Nick Ramsay <NickRamsay1989@gmail.com>
2019-05-29 22:00:06 +1000

    Updated comments to try recommitting

--- dashboardScripts.js
+++ dashboardScripts.js
@@ -33,7 +33,7 @@ function startLocalTime() {
     var t = setTimeout(startLocalTime, 1830);
 }
 function checkLocalTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10 
     return i;
 }
 
