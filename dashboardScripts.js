d8cded80146849735ad91204461d62b3e6421717
Nick Ramsay <NickRamsay1989@gmail.com>
2019-06-05 20:26:05 +1000

    Created separte Daylight Savings time scripts for northern and southern hemispheres, added logic to dashboard JavaScript

--- dashboardScripts.js
+++ dashboardScripts.js
@@ -38,9 +38,25 @@ function checkLocalTime(i) {
 }
 
 function startPortlandTime() {
+    //Daylight Savings Time North automation start...
+    var dst_date = new Date();
+    var dst_month = dst_date.getMonth();
+    var dst_dom = dst_date.getDate(); //Day of Month
+    var dst_dow = dst_date.getDay()+1; //Day of Week, one added to make even with var dom
+
+    var dst_full_months = (dst_month > 3 && dst_month < 9) ? "true":"false"; //Full months of May through September are always DST
+    var first_week = dst_dow <= 7 ? "true":"false"; //True means it's still first week of month
+    var first_sunday = (first_week == "true" && dst_dow < dst_dom) ? "true":"false"; //First Sunday of first week of month has passed
+    var first_sunday_april = (dst_month == 3 && first_sunday == "true") ? "true":"false"; //Month is April and first Sunday has passed
+    var first_sunday_october = (dst_month == 9 && first_sunday == "true") ? "true":"false"; //Month is October and first Sunday has passed
+
+    var dst = (dst_full_months == "true") || ((first_sunday_april == "true") && (first_sunday_october == "false")) ? 1:0; //If months are May through September OR it is after first Sunday of April and before first Sunday of October
+    //Daylight Savings Time North automation end...
+
     var today = new Date();
     var mh = today.getUTCHours();
-    var h = today.getUTCHours()+8;
+    var utc_diff = 7;
+    var h = today.getUTCHours()+utc_diff+dst;
     var m = today.getMinutes();
     var s = today.getSeconds();
     var ampm = h < 12 || h >= 24 ? 'PM' : 'AM';
@@ -58,22 +74,25 @@ function checkPortlandTime(i) {
 }
 
 function startSydneyTime() {
-    //Daylight Savings Time automation start...
-    var dst_date = new Date()
-    var dst_month = dst_date.getMonth()
-    var dst_dom = dst_date.getDate() //Day of Month
-    var dst_dow = dst_date.getDay()+1 //Day of Week, one added to make even with var dom
-    var dst_months = (dst_month > 3 && dst_month < 10) ? "true":"false" //Full months of May through September are always DST
-    var first_week = dst_dow <= 7 ? "true":"false" //True means it's still first week of month
-    var first_sunday = (first_week == "true" && dst_dow < dst_dom) ? "true":"false" //First Sunday of first week of month has passed
-    var first_sunday_april = (dst_month == 3 && first_sunday == "true") ? "true":"false" //Month is April and first Sunday has passed
-    var first_sunday_october = (dst_month == 9 && first_sunday == "true") ? "true":"false" //Month is October and first Sunday has passed
-    var dst = (dst_months == "true") || ((first_sunday_april == "true") && (first_sunday_october == "false")) ? 1:0 //If months are May through September OR it is after first Sunday of April and before first Sunday of October
-    //Daylight Savings Time automation end...
+    //Daylight Savings Time South automation start...
+    var dst_date = new Date();
+    var dst_month = dst_date.getMonth();
+    var dst_dom = dst_date.getDate(); //Day of Month
+    var dst_dow = dst_date.getDay()+1; //Day of Week, one added to make even with var dom
+
+    var dst_full_months = (dst_month > 9 && dst_month < 3) ? "true":"false"; //Full months of November through March are always DST
+    var first_week = dst_dow <= 7 ? "true":"false"; //True means it's still first week of month
+    var first_sunday = (first_week == "true" && dst_dow < dst_dom) ? "true":"false"; //First Sunday of first week of month has passed
+    var first_sunday_april = (dst_month == 3 && first_sunday == "true") ? "true":"false"; //Month is April and first Sunday has passed
+    var first_sunday_october = (dst_month == 9 && first_sunday == "true") ? "true":"false"; //Month is October and first Sunday has passed
+
+    var dst = (dst_full_months == "true") || ((first_sunday_april == "false") && (first_sunday_october == "true")) ? 1:0; //If months are November through March OR it is before first Sunday of April and after first Sunday of October
+    //Daylight Savings Time South automation end...
 
     var today = new Date();
     var mh = today.getUTCHours();
-    var h = today.getUTCHours()+9+dst;
+    var utc_diff = 10;
+    var h = today.getUTCHours()+utc_diff+dst;
     var m = today.getMinutes();
     var s = today.getSeconds();
     var ampm = h > 11 && h < 24 || h >= 36 ? 'PM' : 'AM';
