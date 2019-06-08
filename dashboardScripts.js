e7f7e0947984bd97525ce84f6f4cb7266d0fba9f
Nick Ramsay <NickRamsay1989@gmail.com>
2019-06-08 14:13:40 +1000

    Changed app button to favourites button

--- dashboardScripts.js
+++ dashboardScripts.js
@@ -136,3 +136,26 @@ function aboutModal() {
       }
     }
 }
+
+function appModal() {
+    //Gets the modal
+    var modal = document.getElementById("appModal");
+    //Gets the button that opens the modal
+    var btn = document.getElementById("appBtn");
+    //Get the <span> element that closes modal
+    var span = document.getElementsByClassName("appClose")[0];
+    //When the user clicks the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+}
